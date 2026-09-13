fe63cbcc39ddc0ab0a682271f19117029d11bbc5
dowglasOficial <bittencourtdouglas41@gmail.com>
2026-09-12 23:00:51 -0300

    arquivos base do codigo

---
+++ user-service/src/main/java/com/app/user_service/entity/User.java
@@ -0,0 +1,31 @@
+package com.app.user_service.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity(name = "tb_user")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequence_usuario")
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "data_cadastro", nullable = false)
+    private Date dataCadastro;
+}
---
+++ user-service/src/main/java/com/app/user_service/service/UserService.java
@@ -0,0 +1,12 @@
+package com.app.user_service.service;
+
+import com.app.user_service.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    UserRepository userRepository;
+
+}
